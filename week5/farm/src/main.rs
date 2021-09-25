12feb43f37ffc1ecd388df314a1a8013366696c4
Wang Xingyu <cnspary@outlook.com>
2021-09-25 20:45:50 +0800

    week5 done

--- week5/farm/src/main.rs
+++ week5/farm/src/main.rs
@@ -72,11 +72,35 @@ fn main() {
     let start = Instant::now();
 
     // TODO: call get_input_numbers() and store a queue of numbers to factor
-
+    let nums = Arc::new(Mutex::new(get_input_numbers()));
     // TODO: spawn `num_threads` threads, each of which pops numbers off the queue and calls
     // factor_number() until the queue is empty
+    
+    let mut threads = Vec::new();
+    for _ in 0..num_threads {
+        let nums = nums.clone();
+        threads.push(thread::spawn(move || {
+            loop {
+                let var;
+                
+                {
+                    let mut x = nums.lock().unwrap();
+                    if x.is_empty() {
+                        break;
+                    } else {
+                        var = x.pop_front().unwrap();
+                    }
+                }
+
+                factor_number(var);
+            }
+        }))
+    }
+
 
     // TODO: join all the threads you created
-
+    for handler in threads {
+        handler.join().expect("Panic occured in thread");
+    }
     println!("Total execution time: {:?}", start.elapsed());
 }
